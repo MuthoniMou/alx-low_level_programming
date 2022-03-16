42cce9c4ead6c05f83acb073adb86718f61fe616
MuthoniMou <maureenmuthoni110@gmail.com>
2022-03-16 05:21:24 -0700

    I sometimes suffer from insomnia

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,20 @@
-#include "main.h"                                                                                                                                                                       
-/** 
- * print_alphabet - print the english alphebet from a-z                                              
- *                                                                                                   
- *Return: Nothing.                                                                                  
- */                                                                                                   
-                                                                                                     
-void print_alphabet(void)                                                                       
-{                                                                                                   
+#include "main.h"
+
+/**
+ * print_alphabet - print the english alphebet from a-z
+ *
+ * Return: Nothing.
+ */
+
+void print_alphabet(void)
+{
 	char c = 'a';
 
-	while (c <= 'z')                                                                             
-	{                                                                                            
-		_putchar(c);                                                                         
-		c++;                                                                                 
-	}                                                                                            
-	                                                                                                     
-	_putchar('\n');                                                                              
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+
+	_putchar('\n');
 }
