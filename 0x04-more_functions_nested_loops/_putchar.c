0cff2e58c8d3df3227e7eb89665b7f3b46aff455
MuthoniMou <maureenmuthoni110@gmail.com>
2022-03-17 15:36:31 -0700

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,20 +1,13 @@
-#include "main.h"
+#include <unistd.h>
 
 /**
- * main- Prints _putchar
- * Return: Always 0 (Success)
- */
-int main(void)
+ *  * _putchar - writes the character c to stdout
+ *   * @c: The character to print
+ *    *
+ *     * Return: On success 1.
+ *      * On error, -1 is returned, and errno is set appropriately.
+ *       */
+int _putchar(char c)
 {
-	char text[] = "_putchar";
-	int i = 0;
-
-	while (text[i] != '\0')
-	{
-		_putchar(text[i]);
-		i++;
-	}
-
-	_putchar('\n');
-	return (0);
+		return (write(1, &c, 1));
 }
